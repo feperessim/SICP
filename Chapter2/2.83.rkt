99ec1aabef393a154f395397fb38690119da533d
Felipe <fe.peressim@gmail.com>
2022-04-20 10:50:51 -0300

    Exercise 2.83

---
+++ Chapter2/2.83.rkt
@@ -0,0 +1,13 @@
+(define (raise arg)
+  (apply-generic 'raise arg))
+
+(put 'raise 'integer
+     (lambda (arg) (make-rational arg 1)))
+
+(put 'raise 'rational
+     (lambda (arg) (make-real (/ (number arg) (denom arg)))))
+
+(put 'raise 'real
+     (lambda (arg) (make-from-real-imag arg 0)))
+     
+
